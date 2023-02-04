a98b176501ccd8d23b6766a144bbf4e5c5293752
Minyoung <91351149+Baaam0@users.noreply.github.com>
2023-02-03 20:46:51 -0800

    #

---
+++ .history/e2e/home.mobile.spec_20230203203402.ts
@@ -0,0 +1,10 @@
+import { test, devices, expect } from '@playwright/test';
+
+test.use({
+  browserName: 'chromium',
+  ...devices['iPhone XR']
+})
+
+test.describe("Testing mobile viewport for iPhone XR", () => {
+  test('Testing for colours on iPhone XR')
+})
